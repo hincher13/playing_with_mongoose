e6a15c4c031914c16c4d2c48a6cf94d76a0719ca
Ray Hinch <rahinch@icloud.com>
2023-03-07 15:39:03 -0500

    Test commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import mongoose from 'mongoose';
+import Blog from './model/Blog';
 
 mongoose.connect("mongodb+srv://rahinch:<password>@myfirstcluster.wqg2zea.mongodb.net/?retryWrites=true&w=majority");
+
+// Create new blog post
